e81ec0b0908a1b140b37393879a1b5ccd6448e17
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-28 10:51:21 -0700

    Finish excercise 5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = "John Doe"
+age = 32
+height = 74 * 2.54 # inches
+weight = 180 * 0.45359237 # lbs
+eyes = "Blue"
+teeth = "White"
+hair = "Brown"
+
+puts "Let's talk about #{name}."
+puts "He's #{height} centimeters tall."
+puts "He's #{weight} kilograms heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get my #{age + height + weight}."
