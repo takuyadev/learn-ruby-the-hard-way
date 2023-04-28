d5a40a80e0efa78f39b62a7ed2c4fe3e3343474c
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-28 13:49:45 -0700

    Finish excercise 28

---
+++ ex28.rb
@@ -0,0 +1,17 @@
+puts true && true # true
+puts false && true # false
+puts 1 == 1 && 2 == 1 # false
+puts "test" == "test" # true
+puts 1 == 1 || 2 != 1 # true
+puts "test" == "testing" # false
+puts 1 != 0 && 2 == 1 # false
+puts "test" != "testing" # true
+puts "test" == 1 # false
+puts !(true && false) # true
+puts !(1 == 1 && 0 != 1) # false
+puts !(1 != 0 || 1000 == 1000) # false
+puts !(1 != 10 || 3 == 4) # true
+puts !("testing" == "testing" && "Zed" == "Cool Guy") # true
+puts 1 == 1 && (!("testing" == 1 || 1 == 0)) # true
+puts "chunky" == "bacon" && (!(3 == 4 || 3 == 3)) # false
+puts 3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun")) # false
