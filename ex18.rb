958aaa82c03f38a81a8c2e63c7283877e941bbda
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-28 13:11:09 -0700

    Finish excercise 18

---
+++ ex18.rb
@@ -0,0 +1,25 @@
+# this one is like your scipts with ARGV
+def print_two(*args)
+  arg1, arg2 = args
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2)
+  puts "arg1: #{arg1}, arg2: #{arg2}"
+end
+
+# this just takes one argument
+def print_one(arg1)
+  puts "arg1: #{arg1}"
+end
+
+# this one takes no arguments
+def print_none()
+  puts "I got nothin."
+end
+
+print_two "John", "Doe"
+print_two_again "John", "Doe"
+print_one "John"
+print_none
